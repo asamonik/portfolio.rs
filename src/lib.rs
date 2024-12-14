6a164fb7b328c6a1c55f8f8534130315ba9719df
Andreas <49288593+asamonik@users.noreply.github.com>
2024-12-14 13:19:09 +0100

    feat: hydration (#32)
    Signed-off-by: Andreas <mail@asamonik.at>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use cfg_if::cfg_if;
+use leptos::*;
+use leptos::mount::mount_to_body;
 
 pub mod api;
 pub mod app;
@@ -12,17 +14,32 @@ cfg_if! {
     if #[cfg(feature = "hydrate")] {
         use wasm_bindgen::prelude::wasm_bindgen;
         use app::*;
-        use leptos::*;
 
         #[wasm_bindgen]
         pub fn hydrate() {
             //_ = console_log::init_with_level(log::Level::Debug);
             //console_error_panic_hook::set_once();
-
             log::log!(log::Level::Debug, "hydrate mode - hydrating");
 
-            leptos::mount_to_body(|| {
+            mount_to_body(|| {
                 view! { <App/> }
+            });
+        }
+    }
+    else if #[cfg(feature = "csr")] {
+        use wasm_bindgen::prelude::wasm_bindgen;
+
+        #[wasm_bindgen(start)]
+        pub fn main() {
+            use app::*;
+            use leptos::*;
+            _ = console_log::init_with_level(log::Level::Debug);
+            console_error_panic_hook::set_once();
+
+            log!("csr mode - mounting to body");
+
+            mount_to_body(|cx| {
+                view! { cx, <App /> }
             });
         }
     }
